44aeaec416caac348f8d5f46f3161e205b34459f
Saplyn-Edifice <saplyn@outlook.com>
2023-04-10 19:39:03 +0800

    添加用于教学的错误程序

---
+++ temp.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+void Print()
+{
+    printf("Saplyn is a l'l dog\n");
+}
+
+int main()
+{
+    for (int i = 0; i < 10; i--)
+    {
+        Print();
+    }
+    
+    return 0;
+}
